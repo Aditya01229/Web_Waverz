c11c1172a61be497fe35106f3fe83fb0d43315c1
Om Mayur Pagariya <ompagariya28@gmail.com>
2025-02-05 14:28:58 +0530

    heading

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,18 @@
 export default function Home() {
   return (
     // <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <h1>Main</h1>
+        <section className="mt-24 text-left px-20 py-5">
+          {/* "Trusted Partner For" */}
+          <h2 className="text-7xl font-bold text-black">Trusted <span className="bg-black text-white px-3 py-1 rounded-md">Partner</span> For</h2>
+    
+          {/* "Your Website Development" with styling */}
+          <h1 className="text-7xl font-bold mt-7">
+            <span className="text-black">Your Website </span>
+            <span className="bg-black text-white px-3 py-1 rounded-md">Development.</span>
+          </h1>
+        </section>
+        
+      );
+    }
+    
     // </div>
-  );
-}
