daec8cdccc4805303a2bf0d8fce70f363dd2af3c
Aditya01229 <adityajasoriya2003@gmail.com>
2025-02-07 15:23:13 +0530

    bg error fix

--- src/app/components/service-component.tsx
+++ src/app/components/service-component.tsx
@@ -13,7 +13,7 @@ export default function ServiceComponent({title, description}: ServiceComponentP
     const [isHovered, setIsHovered] = useState(false);
 
     return <>
-        <div className={`bg-black rounded-3xl p-6 items-center flex flex-col gap-8 transition-transform duration-300 ease-in-out ${isHovered ? "bg-[#c5ff41] rotate-1" : ""}`} onMouseEnter={() => setIsHovered(true)}
+        <div className={`rounded-3xl p-6 items-center flex flex-col gap-8 transition-transform duration-300 ease-in-out ${isHovered ? "bg-[#c5ff41] rotate-1" : "bg-black"}`} onMouseEnter={() => setIsHovered(true)}
       onMouseLeave={() => setIsHovered(false)}>
             <div className="w-full flex justify-between">
                 <h3 className={`rounded-3xl p-1 px-6 font-semibold tracking-wide transition-colors duration-100 ${isHovered ? "bg-black text-white" : "bg-[#c9fff0] text-black"}`}>
@@ -24,7 +24,7 @@ export default function ServiceComponent({title, description}: ServiceComponentP
                     <a href="" className="underline">Read More</a>
                 </div>
             </div>
-            <p className={`text-md text-[#bebebe] text-wide ${isHovered ? "text-black" : "text-white"}`}>
+            <p className={`text-md text-wide ${isHovered ? "text-black" : "text-[#bebebe]"}`}>
                 {description}
             </p>
         </div>
