a337cbc4524667382097344f406d70a45b3dedab
Aditya01229 <adityajasoriya2003@gmail.com>
2025-02-08 18:48:42 +0530

    error solve

--- src/app/components/section-heading.tsx
+++ src/app/components/section-heading.tsx
@@ -9,9 +9,9 @@ interface SectionHeadingProps {
 export default function SectionHeading({title, Icon, reqColor}: SectionHeadingProps) {
   return (
     <div className="flex">
-      {/* <div className={`border-[2px] border-${reqColor} rounded-3xl p-3 flex items-center`}>
+      <div className={`border-[2px] border-${reqColor} rounded-3xl p-3 flex items-center`}>
       <Icon className="h-4 w-4" color={reqColor}/>
-      </div> */}
+      </div>
       <p className={`text-${reqColor} border-[2px] border-${reqColor} rounded-3xl p-2 px-6 tracking-[0.2em] text-[1rem]`}>
         {title}
       </p>
